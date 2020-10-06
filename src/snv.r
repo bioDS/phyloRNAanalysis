74e25938d3606ff7aeec02724d33bcf7de506786
Jiří Moravec <jiri.c.moravec@gmail.com>
2020-10-06 17:15:59 +1300

    Changed the snv helper function according to the new python script

--- src/snv.r
+++ src/snv.r
@@ -8,34 +8,34 @@ library("phyloRNA")
 #' This function will perform SNV detection for scRNAseq data.
 #'
 #' To detect SNVs on a scRNAseq, this function first detect a high-quality SNVs by treating
-#' the sample as a bulk sample, then examine each the base frequency at each SNV for every cell
-#' and then finally reports the most frequent base for every cell and every SNV, which will create
-#' the final alignment.
+#' the sample as a bulk sample, then the most common base pertaining to each cell for every
+#' variant is reported and saved to a vcm (variant call matrix) file.
 #'
 #' @param bam an input sam/bam file
 #' @param barcodes a file with cell barcodes
 #' @param reference a reference file to which the bam file was mapped
 #' @param outdir **optional** a general output directory
 #' @param vcfdir **optional** a directory with vcf files
-#' @param vffdir **optional** a directory with vff files
+#' @param vcm **optional** an output vcm file
 #'
 #' @return a path to an alignment table with the most frequent base for every cell at every SNV
 #' position
-detect_snv = function(bam, barcodes, reference, outdir=NULL, vcfdir=NULL, vffdir=NULL){
+detect_snv = function(bam, barcodes, reference, outdir=NULL, vcfdir=NULL, vcm=NULL){
+    core = phyloRNA::corename(bam)
+
     if(phyloRNA::is_nn(outdir))
         outdir = "snv"
     if(phyloRNA::is_nn(vcfdir))
         vcfdir = file.path(outdir, "vcf")
-    if(phyloRNA::is_nn(vffdir))
-        vffdir = file.path(outdir, "vff")
+    if(phyloRNA::is_nn(vcm))
+        vcm = file.path(outdir, paste0(core, ".vcm"))
 
-    core = corename(bam)
+    mkdir(outdir)
+    mkdir(vcfdir)
     vcf = file.path(outdir, paste0(core, ".vcf"))
-    alignment = file.path(outdir, paste0(core, ".alignment.txt"))
 
-    gatk_snv(bam, reference, vcf, vcfdir)
-    vff_make(bam, vcf, barcodes, vffdir)
-    vff_merge(vffdir, alignment)
+    phyloRNA::gatk_snv(bam, reference, vcf, vcfdir)
+    phyloRNA::vcm(bam, vcf, barcodes, output=vcm, nthreads=nthreads)
 
     alignment
     }
