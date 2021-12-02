a17d93e4641f402bf5eaada08616b0c8f895e029
Jiří Moravec <jiri.c.moravec@gmail.com>
2021-12-02 15:49:42 +1300

    Refactored the wang2021 analysis and moved to a single file

---
+++ wang2021.r
@@ -0,0 +1,134 @@
+#' wang2021.r
+#'
+#' Reconstruct phylogenetic trees for data from:
+#' Wang et al. (2021) "Comprehensive analysis of metastatic gastric cancer tumour cells using
+#' single-cell RNA-seq" Sci. Rep.
+import::from("src/sra.r", "get_srr_samples", "srr_download_sample")
+import::from("src/star.r", "STAR")
+import::from("src/snv.r", "detect_snv")
+import::from("src/expr.r", "process_expression")
+import::from("src/iqtree.r", "iqtree")
+import::from("src/beast.r", "beast")
+import::from("src/utils.r", "vcm2fasta", "mdensity")
+import::from("phyloRNA", "gatk_prepare", "abspath", "remove_constant", "tab2seq", "write_fasta")
+import::from("parallel", "mcMap")
+
+main = function(){
+    snv()
+    expr()
+    }
+
+
+snv = function(){
+    outdir = "wang2021"
+    fastqdir = file.path(outdir, "raw")
+    snvdir = file.path(outdir, "snv")
+    refdir = file.path(outdir, "ref")
+    mapdir = file.path(outdir, "map")
+    fastadir = file.path(snvdir, "fasta")
+    treedir = file.path(snvdir, "tree")
+    gse = "GSE158631"
+    
+    # required reference files:
+    reference = "reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+    annotation = "reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gtf"
+    vcf = "reference/00-common_all.vcf.gz"
+    pon = "pon/1000g_pon.hg38.vcf.gz"
+    germline = "af-only-gnomad.hg38.vcf.gz"
+
+    # Download fastqs
+    samples = get_srr_samples(gse, save=file.path(outdir, "samples.rds"))
+    ncores = max(nrow(samples), 16)
+    mcMap(srr_download_sample, samples$srr, samples$name, fastqdir, cellranger=FALSE, mc.cores=ncores)
+    fastqs = file.path(fastqdir, paste0(samples$name, ".fastqs.gz"))
+    fastqs = sapply(fastqs, abspath)
+
+    # Map with STAR
+    mapped = STAR(prefix, fastqs, samples$name, gse, reference, annotation, overhang=50,
+         outdir=outdir, refdir=refdir, mapdir=mapdir)
+
+    # Prepare with GATK
+    prepared = file.path(mapdir, "all.prepared.bam")
+    gatk_prepare(mapped, prepared, reference=reference, vcf=vcf)
+
+    # Detect SNV with Mutect2
+    vcm = detect_snv(prepared, barcodes=NULL, reference, pon=pon, germline=germline, outdir=snvdir)
+
+    # Prepare fasta files
+    names = c("GC1", "GC2", "GC3")
+    n = length(names)
+
+    gc = lapply(names, grep, x=samples$name, value=TRUE)
+    fasta = file.path(fastadir, paste0(names, ".fasta"))
+    mcMap(vcm2fasta, vcm, fasta, gc, mc.cores=n)    
+
+    # run IQtree
+    iqtrees(fasta, "TEST", outdir=file.path(treedir, "ML"), mc.cores=n)
+
+    # run BEAST
+    template = file.path("templates", "BDStrictGtr.xml")
+    beasts(fasta, template, outdir=file.path(treedir, "BI"), mc.cores=n)
+    mcMap(beast, fasta, template, mldir, mc.cores=n)
+    }
+
+
+expr = function(){
+    hdi = c(0.6, 0.9)
+    outdir = file.path("wang2021", "expr")
+    fastadir = file.path(outdir, "fasta")
+    treedir = file.path(outdir, "tree")
+
+    url = paste0("https://www.ncbi.nlm.nih.gov/geo/download/"
+                 "?acc=GSE158631&format=file&file=GSE158631%5Fcount%2Ecsv%2Egz")
+    file = file.path(outdir, "count_matrix.csv.gz")
+    download_file(url, file)
+
+    # Read the data and split it according to patient
+    count_matrix = read.table(infile, sep=",", header=TRUE, row.names=1)
+    count_matrix = split.default(count_matrix, sub("\\..*", "", names(count_matrix)))
+    n = length(count_matrix)
+
+    fasta = file.path(fastadir, paste0(names(count_matrix), ".fasta"))
+    Map(exp2fasta, count_matrix, fasta, summary=TRUE)
+
+    # Run phylogenetic analyses
+    iqtrees(fasta, "ORDERED+ASC", outdir=file.path(treedir, "ML"), mc.cores=n)
+    beasts(fasta, template, outdir=file.path(treedir, "BI"), mc.cores=n)
+    }
+
+
+exp2fasta = function(x, fasta, unknown="-", summary=FALSE){
+    data = press_expression(x, hdi, trim=TRUE, unknown=unknown)
+    data = remove_constant(data, margin=1, unknown=unknown)
+    seq = tab2seq(data, margin=2)
+
+    write_fasta(seq, fasta)
+
+    if(isTRUE(summary))
+        summary = file.path(basename(fasta), paste0(corename(fasta), "_summary.txt"))
+    if(is.character(summary)
+        count_matrix_summary(data, name=corename(fasta), file=summary)
+    }
+
+
+count_matrix_summary = function(data, name=NULL, file=NULL){
+    text = paste0(
+        "Sequences: ", ncol(data), "\n",
+        "Sites: ", nrow(data), "\n",
+        "Unique patterns: ", nrow(unique.matrix(data, MARGIN=1)), "\n",
+        "Data density: ", mdensity(data, empty="-")
+        )
+
+    if(!is.null(name))
+        text = paste0("Name: ", name[1], "\n", text),
+
+    if(!is.null(file))
+        writeLines(text, file)
+
+    return(invisible(text))
+    }
+
+
+if(sys.nframe() == 0){
+    main()
+    }
